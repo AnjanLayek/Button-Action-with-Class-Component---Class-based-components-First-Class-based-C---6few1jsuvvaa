c0aa6344545593807a78d2eb9a3a0b9f85f3d9de
Anjan <anjanlayek001@gmail.com>
2022-11-23 11:49:45 +0530

    ready to commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
 		super(props);
-	};
-
+		this.state={paragraph:null};
+		//this.clickHandler = this.clickHandler.bind(this);
+	}
+   clickHandler(){
+	this.setState({
+		paragraph:
+    <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>   
+	});
+  }
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				{this.state.paragraph}
+				<button id="click" onClick={()=>this.clickHandler()}>Click</button>
+				
     		</div>
     	);
     }
